24ac98f9bf8f16fa098e14c2d90a64a0809cf0a4
nguyenvanduong20140887 <nguyenvanduong.vn.k59@gmail.com>
2017-05-23 16:05:15 +0700

    admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,11 @@ class ApplicationController < ActionController::Base
   def default_url_options
     {locale: I18n.locale}
   end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = t "danger.login"
+      redirect_to root_url
+    end
+  end
 end
